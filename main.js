97b66835dbda6df5f085011a408a952a4f1aa7ee
Gismo2001 <rankenpunk@googlemail.com>
2024-03-09 19:49:22 +0100

    Switcher hinzugefügt

--- main.js
+++ main.js
@@ -12,6 +12,7 @@
 }); */
 
 import './style.css';
+//import { bwBruAndereStyle, bwBruNlwknStyle } from './myStyleJs';
 
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
@@ -26,23 +27,26 @@ import { Vector as VectorSource } from 'ol/source';
 import { Fill, Stroke, Style, Circle } from 'ol/style';
 import { bbox as loadingstrategyBbox } from 'ol/loadingstrategy';
 import Icon from 'ol/style/Icon';
+//import Style from 'ol/style/Style';
 
-//import { bwBruAndereStyle, bwBruNlwknStyle } from './myStyleJs'
+// Import the layer switcher control
+import LayerSwitcher from 'ol-ext/control/LayerSwitcher';
+import Group from 'ol/layer/Group';
 
 const bwBruAndereStyle = new Style({
   image: new Icon({
-    src: './data/bru_andere.svg',
+    src: './data/bru_andere.svg',  // Hier wird der Pfad relativ zur HTML-Datei angenommen
     scale: 0.9,
+    visible: false,
   }),
 });
 
 const bwBruNlwknStyle = new Style({
   image: new Icon({
-    src: './data/bru_nlwkn.svg',
+    src: './data/bru_nlwkn.svg',  // Hier wird der Pfad relativ zur HTML-Datei angenommen
     scale: 0.9,
   }),
 });
-
 
 const attribution = new Attribution({
   collapsible: true,
@@ -60,26 +64,20 @@ const map = new Map({
   controls: defaultControls({ attribution: false }).extend([attribution]),
 });
 
+const switcher = new LayerSwitcher;
+map.addControl(switcher)
+
 const osmTile = new TileLayer({
   title: 'osm',
-  // type: 'base',
+  type: 'base',
   source: new OSM({url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',attributions: ['© OpenStreetMap contributors', 'Tiles courtesy of <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',  ], }),
 });
 
-// WMS gew 1. Ordnung
-const wmsHydErstOrdLayer = new TileLayer({
-  source: new TileWMS({
-    url:  'https://www.umweltkarten-niedersachsen.de/arcgis/services/Hydro_wms/MapServer/WMSServer',
-    params: {
-      'LAYERS': 'Gewässernetz_1._Ordnung29778',
-      'FORMAT': 'image/png',
-      'TRANSPARENT': true,
-    },
-  }),
-  opacity: 1,
-});
+;
 // WMS UESG
 const  wmsUesgLayer = new TileLayer({
+  title: "ÜSG",
+  name: "ÜSG",
   source: new TileWMS({
     url:  'https://www.umweltkarten-niedersachsen.de/arcgis/services/HWSchutz_wms/MapServer/WMSServer',
     params: {
@@ -88,8 +86,38 @@ const  wmsUesgLayer = new TileLayer({
       'TRANSPARENT': true,
     },
   }),
+  visible: false,
+  opacity: .5,
+  
+});
+
+// WMS NSG
+const  wmsNSGLayer = new TileLayer({
+  title: "NSG",
+  name: "NSG",
+  source: new TileWMS({
+    url:  'https://www.umweltkarten-niedersachsen.de/arcgis/services/Natur_wms/MapServer/WMSServer',
+    params: {
+      'LAYERS': 'Naturschutzgebiet',
+      'FORMAT': 'image/png',
+      'TRANSPARENT': true,
+    },
+  }),
+  visible: false,
   opacity: .5,
 });
+
+const gnAtlas2023 = new TileLayer({
+  source: new TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "10", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "2023",
+  opacity: 1,
+  visible: true,
+});
+
 
 /// Gew Layer
 const gewLayer = new Vector({
@@ -129,13 +157,44 @@ const bwBruAndereLayer = new VectorLayer({
   title: 'Brücke (NLWKN)',
   name: 'bruAndere', // Titel für den Layer-Switcher
   style: bwBruAndereStyle, // Verwendung des neuen Stils
-  visible: true,
+  visible: false,
 });
 
-map.addLayer(osmTile)
-map.addLayer(wmsUesgLayer);
-map.addLayer(wmsHydErstOrdLayer);
+const BaseGroup = new Group({
+  title: "Base",
+  fold: true,
+  fold: 'close',
+  layers: [osmTile]
+});
+
+const wmsLayerGroup = new Group({
+  title: "WMS-Lay",
+  visible: false,
+  fold: true,
+  fold: 'close',
+  layers: [wmsUesgLayer, wmsNSGLayer]
+});
+  
+const GNAtlasGroup = new Group({
+  title: "GN-DOP's",
+  visible: false,
+  fold: true,
+  fold: 'close',
+  layers: [ gnAtlas2023 ]
+});
+  
+const BwGroup = new Group({
+  title: "Bauw.",
+  fold: true,
+  fold: 'close',  
+  layers: [ bwBruAndereLayer, bwBruNlwknLayer]
+});
+
+map.addLayer(BaseGroup);
+map.addLayer(GNAtlasGroup);
 map.addLayer(gewLayer);
-map.addLayer(bwBruNlwknLayer);
-map.addLayer(bwBruAndereLayer);
+map.addLayer(wmsLayerGroup);
+map.addLayer(BwGroup);
+
+
 
--- myStyleJs.js
+++ myStyleJs.js
@@ -1,18 +1,14 @@
-import Style from 'ol/style/Style';
-import Icon from 'ol/style/Icon';
-
 const bwBruAndereStyle = new Style({
-    image: new Icon({
-      src: './data/bru_andere.svg',
-      scale: 0.9,
-    }),
-  });
-  
-  const bwBruNlwknStyle = new Style({
-    image: new Icon({
-      src: './data/bru_nlwkn.svg',
-      scale: 0.9,
-    }),
-  });
-  
-  export { bwBruAndereStyle, bwBruNlwknStyle };
+  image: new Icon({
+    src: './data/bru_andere.svg',  // Hier wird der Pfad relativ zur HTML-Datei angenommen
+    scale: 0.9,
+  }),
+});
+
+const bwBruNlwknStyle = new Style({
+  image: new Icon({
+    src: './data/bru_nlwkn.svg',  // Hier wird der Pfad relativ zur HTML-Datei angenommen
+    scale: 0.9,
+  }),
+});
+
