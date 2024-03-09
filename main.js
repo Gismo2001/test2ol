08ed57634e37837c1b4254303edc1af78b29d7d1
Gismo2001 <rankenpunk@googlemail.com>
2024-03-09 15:45:51 +0100

    tile layer hinzugefügt

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 import './style.css';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
+import { Vector } from 'ol/layer';
+import { GeoJSON } from 'ol/format';
 import { fromLonLat } from 'ol/proj';
 import OSM from 'ol/source/OSM';
-import { defaults as defaultControls } from 'ol/control';
-import { Vector } from 'ol/layer';
+import { Attribution, defaults as defaultControls } from 'ol/control';
 import { Vector as VectorSource } from 'ol/source';
-import { GeoJSON } from 'ol/format';
 import { Fill, Stroke, Style } from 'ol/style';
 import { bbox as loadingstrategyBbox } from 'ol/loadingstrategy';
+
+const attribution = new Attribution({
+  collapsible: true,
+});
 
 const mapView = new View({
   center: fromLonLat([7.35, 52.7]),
@@ -18,7 +23,8 @@ const mapView = new View({
 const map = new Map({
   target: 'map',
   view: mapView,
-  controls: defaultControls().extend([]), // Falls erforderlich
+  //controls: defaultControls().extend([]), // Falls erforderlich
+  controls: defaultControls({ attribution: false }).extend([attribution]),
 });
 
 const osmTile = new TileLayer({
@@ -27,7 +33,35 @@ const osmTile = new TileLayer({
   source: new OSM({url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',attributions: ['© OpenStreetMap contributors', 'Tiles courtesy of <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',  ], }),
 });
 
-map.addLayer(osmTile);
+
+// WMS Layer
+const wmsLayer = new TileLayer({
+  source: new TileWMS({
+    url: 'http://sg.geodatenzentrum.de/wms_dlm250_inspire',
+    params: {
+      'LAYERS': 'HY.Network.WatercourseLink',
+      'FORMAT': 'image/png',
+      'TRANSPARENT': true,
+    },
+  }),
+  opacity: 1,
+});
+
+/* var layer_HYNetworkWatercourseLink_0 = L.WMS.layer("http://sg.geodatenzentrum.de/wms_dlm250_inspire", "HY.Network.WatercourseLink", {
+  pane: 'pane_HYNetworkWatercourseLink_0',
+  format: 'image/png',
+  uppercase: true,
+  transparent: true,
+  continuousWorld : true,
+  tiled: true,
+  info_format: 'text/html',
+  opacity: 1,
+  identify: false,
+  attribution: '',
+}); */
+map.addLayer(osmTile)
+
+map.addLayer(wmsLayer);
 
 /// Gew Layer
 const gewLayer = new Vector({
@@ -45,3 +79,5 @@ const gewLayer = new Vector({
 });
 
 map.addLayer(gewLayer);
+
+map.addLayer(osmTile)
