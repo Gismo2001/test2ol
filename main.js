ee741895e717da57e80afa520490ea4d0e95ee9a
Gismo2001 <rankenpunk@googlemail.com>
2024-03-23 14:19:31 +0100

    koordinatenanzeige hinzugefügt

--- main.js
+++ main.js
@@ -25,8 +25,17 @@ import {unByKey} from 'ol/Observable.js';
 import { FullScreen, Attribution, defaults as defaultControls, ZoomToExtent, Control } from 'ol/control.js';
 import { DragRotateAndZoom } from 'ol/interaction.js';
 import { defaults as defaultInteractions } from 'ol/interaction.js';
+
 import MousePosition from 'ol/control/MousePosition.js';
+import { transform } from 'ol/proj';
 import {createStringXY} from 'ol/coordinate.js';
+import { register } from 'ol/proj/proj4';
+import proj4 from 'proj4';
+
+//projektion definieren und registrieren
+proj4.defs('EPSG:32632', '+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs');
+register(proj4);
+
 
 import LayerSwitcher from 'ol-ext/control/LayerSwitcher';
 import LayerGroup from 'ol/layer/Group';
@@ -139,11 +148,99 @@ const map = new Map({
   ]),
   interactions: defaultInteractions().extend([new DragRotateAndZoom()])
 });
-/* var mousePosition = new MousePosition({
-  coordinateFormat: createStringXY(4),
-  projection: 'EPSG:4326'
+
+const mousePositionControl = new MousePosition({
+  coordinateFormat: createStringXY(6),
+  projection: 'EPSG:4326', // Start-Projektion
+  className: 'custom-mouse-position',
+  target: document.getElementById('mouse-position'),
 });
-map.addControl(mousePosition); */
+map.addControl(mousePositionControl);
+// Projektion für Maus anwenden
+const projectionSelect = document.getElementById('projecSelect');
+projectionSelect.addEventListener('change', function (event) {
+  if (projectionSelect.value === 'EPSG:3857') {
+    const format = createStringXY(2);
+    mousePositionControl.setCoordinateFormat(format);
+    mousePositionControl.setProjection(event.target.value);
+    
+  } else if (projectionSelect.value === 'EPSG:4326') {
+    const format = createStringXY(6);
+    mousePositionControl.setCoordinateFormat(format);
+    mousePositionControl.setProjection(event.target.value);
+
+  } else if (projectionSelect.value === 'EPSG:32632') {
+    console.log('32632')
+    const format = createStringXY(1);
+    mousePositionControl.setCoordinateFormat(format);
+    mousePositionControl.setProjection(event.target.value);
+  }
+});
+function placeMarkerAndShowCoordinates(event) {
+  map.getOverlays().clear();
+  const mousePositionElement = document.getElementById('mouse-position'); // Auswahl des HTML-Elements
+  if (toggleCheckbox.checked) {
+    const marker = document.createElement('div');
+    marker.className = 'marker';
+    const markerOverlay = new Overlay({
+      position: event.coordinate,
+      positioning: 'center-center', 
+      element: marker,
+      stopEvent: false,
+    });
+    map.addOverlay(markerOverlay);
+    if (projectionSelect.value === 'EPSG:3857') {
+      const format = createStringXY(2);
+      mousePositionElement.innerHTML = `Coordinates: ${format(event.coordinate)}`;
+    } else if (projectionSelect.value === 'EPSG:4326') {
+      const format = createStringXY(6);
+      const transformedCoordinate = transformCoordinateToMousePosition4326(event.coordinate);
+      mousePositionElement.innerHTML = `Coordinates: ${format(transformedCoordinate)}`;
+    } else if (projectionSelect.value === 'EPSG:32632') {
+      console.log('32632')
+      const format = createStringXY(1);
+      const transformedCoordinate = transformCoordinateToMousePosition32632(event.coordinate);
+      mousePositionElement.innerHTML = `Coordinates: ${format(transformedCoordinate)}`;
+      //const googleMapsLink = `https://maps.app.goo.gl/?q=${transformedCoordinate[0]},${transformedCoordinate[1]}`;
+      //console.log(googleMapsLink);
+    }
+  }
+}
+//map.on('click', placeMarkerAndShowCoordinates);
+const toggleCheckbox = document.getElementById('toggle-checkbox');
+toggleCheckbox.addEventListener('change', function() {
+  if (this.checked) {
+    document.getElementById('mouse-position').innerHTML = "";
+    map.removeControl(mousePositionControl); 
+  } else {
+    map.addControl(mousePositionControl);
+  }
+});
+document.getElementById('hide-button').addEventListener('click', function() {
+  const controls = document.querySelector('.controls');
+  controls.classList.toggle('hidden');
+  if (controls.classList.contains('hidden')) {
+    //map.getOverlays().clear();
+    document.getElementById('toggle-checkbox').checked = false;
+    map.un('click', placeMarkerAndShowCoordinates);
+  } else {
+    map.addControl(mousePositionControl);
+    //map.on('click', placeMarkerAndShowCoordinates);
+  }
+});
+//Umrechnung geclickter Kartenpositionen in mousePositionControl-Format
+//für EPSG:4326
+function transformCoordinateToMousePosition4326(coordinate) {
+  // Koordinaten in das Format von mousePositionControl (EPSG:4326) umwandeln
+  return transform(coordinate, map.getView().getProjection(), 'EPSG:4326');
+}
+//für EPSG:32632
+function transformCoordinateToMousePosition32632(coordinate) {
+  // Koordinaten von der aktuellen Karte (EPSG:3857) nach EPSG:32632 umwandeln
+  //  const transformedCoordinate32632 = transform(clickedCoordinate3857, 'EPSG:3857', 'EPSG:32632');
+  return transform(coordinate, 'EPSG:3857', 'EPSG:32632');
+}
+
 
 const sourceP = new VectorSource();
 let layerP = null; // Initial kein Layer vorhanden
